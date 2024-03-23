ec4b232b3d731af0d96fd6e965953a566232651d
Caleb Nicholas <106997882+cjnich@users.noreply.github.com>
2024-03-23 08:43:22 -0600

    chore: display only admin routes from sidebar

--- src/components/sidebar/components/Content.tsx
+++ src/components/sidebar/components/Content.tsx
@@ -3,19 +3,27 @@ import { Box, Flex, Stack } from '@chakra-ui/react';
 //   Custom components
 import Brand from 'components/sidebar/components/Brand';
 import Links from 'components/sidebar/components/Links';
-import SidebarCard from 'components/sidebar/components/SidebarCard';
+// import SidebarCard from 'components/sidebar/components/SidebarCard';
 
 // FUNCTIONS
 
-function SidebarContent(props: { routes: RoutesType[] }) {
+const SidebarContent = (props: { routes: RoutesType[] }) => {
+	
 	const { routes } = props;
-	// SIDEBAR
+
+	//TODO: Move to store context
+	const displayedRoutes = routes.filter((route) => route.layout === '/admin');
+	
 	return (
-		<Flex direction='column' height='100%' pt='25px' borderRadius='30px'>
+		<Flex 
+			direction='column' 
+			alignItems={{ base: 'center', lg: 'flex-start' }}
+			height='100%' 
+			borderRadius='30px'>
 			<Brand />
 			<Stack direction='column' mt='8px' mb='auto'>
 				<Box ps='20px' pe={{ lg: '16px', '2xl': '16px' }}>
-					<Links routes={routes} />
+					<Links routes={displayedRoutes} />
 				</Box>
 			</Stack>
 
