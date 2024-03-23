1794fc673e52653253a2f8b0bf271698a42d69ca
Caleb Nicholas <106997882+cjnich@users.noreply.github.com>
2024-03-23 07:37:07 -0600

    chore: remove brand logo from sidebar

--- src/components/sidebar/components/Brand.tsx
+++ src/components/sidebar/components/Brand.tsx
@@ -1,18 +1,21 @@
 // Chakra imports
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { 
+	Flex, 
+	// useColorModeValue 
+} from '@chakra-ui/react';
 
 // Custom components
-import { HorizonLogo } from 'components/icons/Icons';
+// import { HorizonLogo } from 'components/icons/Icons';
 import { HSeparator } from 'components/separator/Separator';
 
 export function SidebarBrand() {
 	//   Chakra color mode
-	let logoColor = useColorModeValue('navy.700', 'white');
+	// let logoColor = useColorModeValue('navy.700', 'white');
 
 	return (
 		<Flex alignItems='center' flexDirection='column'>
-			<HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
-			<HSeparator mb='20px' />
+			{/* <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} /> */}
+			<HSeparator mb='20px' mt='80px' />
 		</Flex>
 	);
 }
